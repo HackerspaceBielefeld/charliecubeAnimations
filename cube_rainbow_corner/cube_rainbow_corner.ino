1f8a87e006890aa747282b4c5e18856161429d50
tauli <taulmarill@xgn.de>
2017-02-11 13:31:28 +0100

    fixed corner to corner transitions

--- cube_rainbow_corner/cube_rainbow_corner.ino
+++ cube_rainbow_corner/cube_rainbow_corner.ino
@@ -16,12 +16,12 @@ void drawCube() {
   for (byte x = 0; x <= 3; x++) {
     for (byte y = 0; y <= 3; y++) {
       for (byte z = 0; z <= 3; z++) {
-        byte xx = shift[0] > x ? 0 : x - shift[0];
-        byte yy = shift[1] > x ? 0 : y - shift[1];
-        byte zz = shift[2] > x ? 0 : z - shift[2];
-        xx = mirror[0] ? 3 - xx : xx;
-        yy = mirror[1] ? 3 - yy : yy;
-        zz = mirror[2] ? 3 - zz : zz;
+        byte xx = mirror[0] ? 3 - x : x;
+        byte yy = mirror[1] ? 3 - y : y;
+        byte zz = mirror[2] ? 3 - z : z;
+        xx = shift[0] > xx ? 0 : xx - shift[0];
+        yy = shift[1] > yy ? 0 : yy - shift[1];
+        zz = shift[2] > zz ? 0 : zz - shift[2];
         byte val = (offset + xx + yy + zz) % 24;
         drawLed(red,  spectrum[val][0],x,y,z);
         drawLed(green,spectrum[val][1],x,y,z);
@@ -32,7 +32,7 @@ void drawCube() {
   flushBuffer();
   clearBuffer();
   offset = offset == 0 ? 23 : offset - 1;
-  delay(100);  
+  delay(90);  
 }
 
 void moveAxis(byte axis) {
